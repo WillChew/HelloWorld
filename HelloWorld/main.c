47e135344db22746f5e546fbcc3e6385b2070b4d
WillChew <williamchew95@gmail.com>
2018-05-21 16:08:04 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Will Chew on 2018-05-21.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
